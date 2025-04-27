56c0e9947e2c1ea602cdd936207522f686cace2d
Andydevs <akanshul97@gmail.com>
2025-04-27 18:37:40 -0400

    Make the jamaican bot

--- app.js
+++ app.js
@@ -1,65 +1,89 @@
-import 'dotenv/config';
-import express from 'express';
+import "dotenv/config";
+import express from "express";
 import {
-  ButtonStyleTypes,
-  InteractionResponseFlags,
-  InteractionResponseType,
-  InteractionType,
-  MessageComponentTypes,
-  verifyKeyMiddleware,
-} from 'discord-interactions';
-import { getRandomEmoji, DiscordRequest } from './utils.js';
-import { getShuffledOptions, getResult } from './game.js';
+    InteractionResponseType,
+    InteractionType,
+    verifyKeyMiddleware,
+} from "discord-interactions";
+import { getRandomEmoji } from "./utils.js";
+import OpenAI from "openai";
 
 // Create an express app
 const app = express();
 // Get port, or default to 3000
 const PORT = process.env.PORT || 3000;
-// To keep track of our active games
-const activeGames = {};
+
+// Get open AI
+let client = new OpenAI()
+
+async function doTranslate(message) {
+    const response = await client.responses.create({
+        model: "gpt-4.1-nano",
+        input: `Translate the following into Jamaican Patois: "${message}"`
+    });
+    return response.output_text
+}
 
 /**
  * Interactions endpoint URL where Discord will send HTTP requests
  * Parse request body and verifies incoming requests using discord-interactions package
  */
-app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async function (req, res) {
-  // Interaction id, type and data
-  const { id, type, data } = req.body;
+app.post(
+    "/interactions",
+    verifyKeyMiddleware(process.env.PUBLIC_KEY),
+    async function (req, res) {
+        // Interaction id, type and data
+        const { id, type, data } = req.body;
 
-  /**
-   * Handle verification requests
-   */
-  if (type === InteractionType.PING) {
-    return res.send({ type: InteractionResponseType.PONG });
-  }
+        /**
+         * Handle verification requests
+         */
+        if (type === InteractionType.PING) {
+            return res.send({ type: InteractionResponseType.PONG });
+        }
 
-  /**
-   * Handle slash command requests
-   * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
-   */
-  if (type === InteractionType.APPLICATION_COMMAND) {
-    const { name } = data;
+        /**
+         * Handle slash command requests
+         * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
+         */
+        if (type === InteractionType.APPLICATION_COMMAND) {
+            const { name } = data;
 
-    // "test" command
-    if (name === 'test') {
-      // Send a message into the channel where command was triggered from
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          // Fetches a random emoji to send from a helper function
-          content: `hello world ${getRandomEmoji()}`,
-        },
-      });
+            // "test" command
+            if (name === "test") {
+                // Send a message into the channel where command was triggered from
+                return res.send({
+                    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                    data: {
+                        // Fetches a random emoji to send from a helper function
+                        content: `hello world ${getRandomEmoji()}`,
+                    },
+                });
+            }
+            // Translate command
+            else if (name == 'translate') {
+                const message = data.options[0].value
+                let translated = await doTranslate(message)
+
+                // Send translated message
+                return res.send({
+                    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                    data: {
+                        content: translated,
+                    },
+                });
+
+            }
+
+            console.error(`unknown command: ${name}`);
+            return res.status(400).json({ error: "unknown command" });
+        }
+
+        console.error("unknown interaction type", type);
+        return res.status(400).json({ error: "unknown interaction type" });
     }
-
-    console.error(`unknown command: ${name}`);
-    return res.status(400).json({ error: 'unknown command' });
-  }
-
-  console.error('unknown interaction type', type);
-  return res.status(400).json({ error: 'unknown interaction type' });
-});
+);
 
 app.listen(PORT, () => {
-  console.log('Listening on port', PORT);
+    console.log("Listening on port", PORT);
 });
--- commands.js
+++ commands.js
@@ -1,49 +1,48 @@
-import 'dotenv/config';
-import { getRPSChoices } from './game.js';
-import { capitalize, InstallGlobalCommands } from './utils.js';
+import "dotenv/config";
+import { getRPSChoices } from "./game.js";
+import { capitalize, InstallGlobalCommands } from "./utils.js";
 
 // Get the game choices from game.js
 function createCommandChoices() {
-  const choices = getRPSChoices();
-  const commandChoices = [];
+    const choices = getRPSChoices();
+    const commandChoices = [];
 
-  for (let choice of choices) {
-    commandChoices.push({
-      name: capitalize(choice),
-      value: choice.toLowerCase(),
-    });
-  }
+    for (let choice of choices) {
+        commandChoices.push({
+            name: capitalize(choice),
+            value: choice.toLowerCase(),
+        });
+    }
 
-  return commandChoices;
+    return commandChoices;
 }
 
 // Simple test command
 const TEST_COMMAND = {
-  name: 'test',
-  description: 'Basic command',
-  type: 1,
-  integration_types: [0, 1],
-  contexts: [0, 1, 2],
+    name: "test",
+    description: "Basic command",
+    type: 1,
+    integration_types: [0, 1],
+    contexts: [0, 1, 2],
 };
 
-// Command containing options
-const CHALLENGE_COMMAND = {
-  name: 'challenge',
-  description: 'Challenge to a match of rock paper scissors',
-  options: [
-    {
-      type: 3,
-      name: 'object',
-      description: 'Pick your object',
-      required: true,
-      choices: createCommandChoices(),
-    },
-  ],
-  type: 1,
-  integration_types: [0, 1],
-  contexts: [0, 2],
-};
+// Translate command
+const TRANSLATE_COMMAND = {
+    name: 'translate',
+    description: "Translate into patois",
+    options: [
+        {
+            type: 3,
+            name: "message",
+            description: "Enter what to translate",
+            required: true
+        }
+    ],
+    type: 1,
+    integration_types: [0, 1],
+    contexts: [0, 2]
+}
 
-const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND];
+const ALL_COMMANDS = [TEST_COMMAND, TRANSLATE_COMMAND];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
